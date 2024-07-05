e907c073e19cbf5b305e4bfc3b137a820f29bc8c
dipendujana5 <your-email@example.com>
2024-07-06 01:54:48 +0530

    add links

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -42,13 +42,13 @@ function Hero() {
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
+          <a href="https://x.com/DipenduJana05" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/dipendujana" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/dipendu-jana-36494420a/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
